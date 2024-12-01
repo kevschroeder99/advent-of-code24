0a8b40c6f2405822000ed206d471dae7668decff
Schröder, Kevin <kevin.schroeder@hdi.de>
2024-12-01 14:11:41 +0100

    Added Day 1 Part 1 and Two

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,15 @@
+import day01.DistanceTracker;
+import day01.SimilarityScore;
+
 public class Main {
 
-    public static void main(String[] args) {
-        System.out.println("Advent of Code!");
+    public static void main(String[] args) throws Exception {
+
+        // Day 1
+        DistanceTracker distanceTracker = new DistanceTracker();
+        System.out.println("Day 1#1: " + distanceTracker.getDistance());
+        SimilarityScore similarityScore = new SimilarityScore();
+        System.out.println("Day 1#2: " + similarityScore.determineSimilarity());
+
     }
 }
---
+++ src/main/java/day01/DistanceTracker.java
@@ -0,0 +1,50 @@
+package day01;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Distance Tracker ist Part 1 of Day 1.
+ */
+public class DistanceTracker {
+
+    List<Integer> distanceList = new ArrayList<>();
+
+    public static final String PATH_TO_INPUT = "src/main/resources/inputs/input_day01.txt";
+
+    public Integer getDistance() throws IOException {
+        LocationList locations1 = new LocationList();
+        LocationList locations2 = new LocationList();
+        try (BufferedReader br = new BufferedReader(new FileReader(PATH_TO_INPUT))) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                String[] splitted = line.split("\\s+");
+                locations1.add(Integer.parseInt(splitted[0]));
+                locations2.add(Integer.parseInt(splitted[1]));
+            }
+        }
+
+        getDistanceFromLocations(locations1, locations2);
+        return distanceList.stream().mapToInt(Integer::intValue).sum();
+    }
+
+    // Recursive call
+    private Integer getDistanceFromLocations(LocationList locations1, LocationList locations2) {
+        if (!locations1.locationIds.isEmpty()) {
+            int locationId1 = locations1.getSmallestLocationId();
+            int locationId2 = locations2.getSmallestLocationId();
+            if (locationId1 > locationId2) {
+                distanceList.add(locationId1 - locationId2);
+            } else {
+                distanceList.add(locationId2 - locationId1);
+            }
+            locations1.remove(locations1.getSmallestLocationId());
+            locations2.remove(locations2.getSmallestLocationId());
+            return getDistanceFromLocations(locations1, locations2);
+        }
+        return 0;
+    }
+}
